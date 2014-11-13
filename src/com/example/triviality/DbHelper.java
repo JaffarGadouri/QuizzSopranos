cc1a3cae414debc9818ef60cada1a5c00b79bc29
stagiaire <stagiaire@Mexico6-PC>
2014-11-13 17:05:27 +0100

    first

---
+++ src/com/example/triviality/DbHelper.java
@@ -0,0 +1,104 @@
+package com.example.triviality;
+import java.util.ArrayList;
+import java.util.List;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+public class DbHelper extends SQLiteOpenHelper {
+	private static final int DATABASE_VERSION = 1;
+	// Database Name
+	private static final String DATABASE_NAME = "triviaQuiz";
+	// tasks table name
+	private static final String TABLE_QUEST = "quest";
+	// tasks Table Columns names
+	private static final String KEY_ID = "id";
+	private static final String KEY_QUES = "question";
+	private static final String KEY_ANSWER = "answer"; //correct option
+	private static final String KEY_OPTA= "opta"; //option a
+	private static final String KEY_OPTB= "optb"; //option b
+	private static final String KEY_OPTC= "optc"; //option c
+	private SQLiteDatabase dbase;
+	public DbHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		dbase=db;
+		String sql = "CREATE TABLE IF NOT EXISTS " + TABLE_QUEST + " ( "
+				+ KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + KEY_QUES
+				+ " TEXT, " + KEY_ANSWER+ " TEXT, "+KEY_OPTA +" TEXT, "
+				+KEY_OPTB +" TEXT, "+KEY_OPTC+" TEXT)";
+		db.execSQL(sql);		
+		addQuestions();
+		//db.close();
+	}
+	private void addQuestions()
+	{
+		Question q1=new Question("Who created the TV show" +
+				" The Sopranos ?","Martin Scorsese", "James Gandolfini", "David Chase", "David Chase");
+		this.addQuestion(q1);
+		Question q2=new Question("Where the majority of the exterior" +
+				" scenes take place?", "New York", "New Jersey", "Sicilia", "New Jersey");
+		this.addQuestion(q2);
+		Question q3=new Question("Who played Dr.Melfi ?","Lorraine Bracco", "Nancy Marchand","Edie Falco","Lorraine Bracco");
+		this.addQuestion(q3);
+		Question q4=new Question("How many seasons" +
+				" do we got?",	"6", "4", "8","6");
+		this.addQuestion(q4);
+		Question q5=new Question("When was the HBO premiere" +
+				" of The Sopranos?","January 30, 2000","January 20, 1998","January 10, 1999","January 10, 1999");
+		this.addQuestion(q5);
+	}
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldV, int newV) {
+		// Drop older table if existed
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_QUEST);
+		// Create tables again
+		onCreate(db);
+	}
+	// Adding new question
+	public void addQuestion(Question quest) {
+		//SQLiteDatabase db = this.getWritableDatabase();
+		ContentValues values = new ContentValues();
+		values.put(KEY_QUES, quest.getQUESTION()); 
+		values.put(KEY_ANSWER, quest.getANSWER());
+		values.put(KEY_OPTA, quest.getOPTA());
+		values.put(KEY_OPTB, quest.getOPTB());
+		values.put(KEY_OPTC, quest.getOPTC());
+		// Inserting Row
+		dbase.insert(TABLE_QUEST, null, values);		
+	}
+	public List<Question> getAllQuestions() {
+		List<Question> quesList = new ArrayList<Question>();
+		// Select All Query
+		String selectQuery = "SELECT  * FROM " + TABLE_QUEST;
+		dbase=this.getReadableDatabase();
+		Cursor cursor = dbase.rawQuery(selectQuery, null);
+		// looping through all rows and adding to list
+		if (cursor.moveToFirst()) {
+			do {
+				Question quest = new Question();
+				quest.setID(cursor.getInt(0));
+				quest.setQUESTION(cursor.getString(1));
+				quest.setANSWER(cursor.getString(2));
+				quest.setOPTA(cursor.getString(3));
+				quest.setOPTB(cursor.getString(4));
+				quest.setOPTC(cursor.getString(5));
+				quesList.add(quest);
+			} while (cursor.moveToNext());
+		}
+		// return quest list
+		return quesList;
+	}
+	public int rowcount()
+	{
+		int row=0;
+		String selectQuery = "SELECT  * FROM " + TABLE_QUEST;
+		SQLiteDatabase db = this.getWritableDatabase();
+		Cursor cursor = db.rawQuery(selectQuery, null);
+		row=cursor.getCount();
+		return row;
+	}
+}
---
+++ src/com/example/triviality/ResultActivity.java
@@ -0,0 +1,62 @@
+package com.example.triviality;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class ResultActivity extends Activity implements OnClickListener{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_result);
+		// get rating bar object
+		RatingBar bar = (RatingBar) findViewById(R.id.ratingBar1);
+		bar.setNumStars(5);
+		bar.setStepSize(0.5f);
+		// get text view
+		TextView t = (TextView) findViewById(R.id.textResult);
+		// get score
+		Bundle b = getIntent().getExtras();
+		int score = b.getInt("score");
+		// display score
+		bar.setRating(score);
+		switch (score) {
+		case 1:
+		case 2:
+			t.setText("Oopsie! Better Luck Next Time! "+score);
+			break;
+		case 3:
+		case 4:
+			t.setText("Hmmmm.. Someone's been reading a lot of trivia "+score);
+			break;
+		case 5:
+			t.setText("Who are you? A trivia wizard??? "+score);
+			break;
+		}
+		Button menuBouton = (Button) findViewById(R.id.backbttn);
+        menuBouton.setOnClickListener(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_result, menu);
+		return true;
+	}
+
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		Intent intent = new Intent(this, com.example.triviality.QuizActivity.class);
+		
+		startActivity(intent);
+	}
+}
